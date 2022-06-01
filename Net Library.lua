f9bb6cc167b31db38a52ecaa3aa196644a6e5377
your4eyes <99973263+your4eyes@users.noreply.github.com>
2022-06-01 07:47:56 -0400

    Update Net Library.lua
    edited a bit

--- Net Library.lua
+++ Net Library.lua
@@ -1,15 +1,16 @@
+repeat task.wait() until game:IsLoaded()
+local Players = game:GetService("Players") --define variables n shit
+local RunService = game:GetService("RunService")
+local LocalPlayer = Players.LocalPlayer
 --[[
 Network Library by 4eyes
 Usage: Put this in your script and use Network.RetainPart(Part) on any part you'd like to retain ownership over, then just apply a replicating method of movement. Credit me if you'd like.
 --]]
-local Players = game:GetService("Players") --define variables n shit
-local RunService = game:GetService("RunService")
-local LocalPlayer = Players.LocalPlayer
-if not getrenv().Network then
-	getrenv().Network = {}
+if not getgenv().Network then
+	getgenv().Network = {}
 	Network["BaseParts"] = {}
 	Network["RetainPart"] = function(Part) --function for retaining ownership of unanchored parts
-		if Part:IsA("BasePart") and isnetworkowner(Part) then
+		if Part:IsA("BasePart") and not isnetworkowner(Part) then
 			local CParts = Part:GetConnectedParts()
 			for _,CPart in pairs(CParts) do --check if part is connected to anything already in baseparts being retained
 				if table.find(Network["BaseParts"],CPart) then
@@ -17,11 +18,17 @@ if not getrenv().Network then
 					return
 				end
 			end
+			local BV = Instance.new("BodyVelocity") --create bodyvelocity to apply constant physics packets and retain ownership
+			BV.Name = "NetworkRetainer"
+        		BV.MaxForce = Vector3.new(1/0,1/0,1/0)
+			BV.P = 1/0
+			BV.Velocity = Vector3.new(30,30,30)
+			BV.Parent = Part
 			table.insert(Network["BaseParts"],Part)
 		end
 	end
 	Network["SuperStepper"] = Instance.new("BindableEvent") --make super fast event to connect to
-	game:DefineFastFlag("NewRunServiceSignals",true)
+	setfflag("NewRunServiceSignals","true")
 	for _,Event in pairs({RunService.RenderStepped,RunService.Heartbeat,RunService.Stepped,RunService.PreSimulation,RunService.PostSimulation}) do
 		Event:Connect(function()
 			return Network["SuperStepper"]:Fire(Network["SuperStepper"],tick())
@@ -32,27 +39,25 @@ if not getrenv().Network then
 	Network["PartOwnership"]["Execute"] = coroutine.create(function() --creating a thread for network stuff
 		if Network["PartOwnership"]["Enabled"] == false then
 			Network["PartOwnership"]["Enabled"] = true --do cool network stuff before doing more cool network stuff
-			setscriptable(workspace,"StreamingTargetRadius",true)
 			setscriptable(workspace,"PhysicsSteppingMethod",true)
-			setscriptable(workspace,"StreamingMinRadius",true)
-			setscriptable(workspace,"StreamOutBehavior",true)
+			setscriptable(workspace,"PhysicsSimulationRateReplicator",true)
 			settings().Physics.PhysicsEnvironmentalThrottle = Enum.EnviromentalPhysicsThrottle.Disabled
+			workspace.PhysicsSimulationRateReplicator = Enum.PhysicsSimulationRate.Fixed240Hz
 			workspace.InterpolationThrottling = Enum.InterpolationThrottlingMode.Disabled
 			workspace.PhysicsSteppingMethod = Enum.PhysicsSteppingMethod.Fixed
-			workspace.StreamOutBehavior = Enum.StreamOutBehavior.Opportunistic
-			sethiddenproperty(LocalPlayer,"MaximumSimulationRadius",1/0)
-			workspace.StreamingTargetRadius = 1/0
+			LocalPlayer.ReplicationFocus = workspace
 			settings().Physics.AllowSleep = false
-			workspace.StreamingMinRadius = 1/0
-			workspace.StreamingEnabled = true
 			Network["SuperStepper"].Event:Connect(function() --super fast asynchronous loop
 				sethiddenproperty(LocalPlayer,"SimulationRadius",1/0)
 				for _,Part in pairs(Network["BaseParts"]) do --loop through parts and do network stuff
 					coroutine.wrap(function()
-						Part.Velocity = Vector3.new(14.465,14.465,14.465)
-						sethiddenproperty(Part,"NetworkIsSleeping",false)
-						LocalPlayer.ReplicationFocus = Part
-						--[==[ [[by 4eyes btw]] ]==]
+						if not isnetworkowner(Part) then --lag parts my ownership is contesting but dont have network over to spite the people who have ownership of stuff i want >:(
+							print("[NETWORK] Part "..Part:GetFullName().." is not owned. Contesting ownership...")
+							sethiddenproperty(Part,"NetworkIsSleeping",true)
+						else
+							sethiddenproperty(Part,"NetworkIsSleeping",false)
+						end
+						--[==[ [[by 4eyes btw]] ]==]--
 					end)()
 				end
 			end)
