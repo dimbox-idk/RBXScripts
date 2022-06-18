3f3f3e7e4e959e8b46e08ced84d185088d8b726d
your4eyes <99973263+your4eyes@users.noreply.github.com>
2022-06-18 19:05:40 -0400

    Update Net_Library.lua

--- Net_Library.lua
+++ Net_Library.lua
@@ -6,7 +6,7 @@ Network Library by 4eyes
 
 The basic concepts of Network Ownership for anyone interested:
 1. Parts not network owned by server or another player will be owned by the player closest to it.
-2. To retain network ownership, you must be constantly sending physics packets or people may be able to take ownership, as your network is contested when you aren't sending physics packets.
+2. To retain network ownership, you must be CONSTANTLY sending physics packets (if you lag you are not sending them) or people may be able to take ownership, as your network is contested when you aren't sending physics packets.
 
 Usage: Put this in your script, run the PartOwnership enable coroutine [ coroutine.resume(Network["PartOwnership"]["Enable"]) ] and use Network.RetainPart(Part) on any part you'd like to retain ownership over, then just apply a replicating method of movement. Network["RemovePart"](Part) to remove ownership of a part, and run the PartOwnership Disable coroutine [ coroutine.resume(Network["PartOwnership"]["Disable"]) ] to stop. Set Network.CharacterRelative to false if you change your character to a non-replicating client model. Credit me if you'd like.
 
