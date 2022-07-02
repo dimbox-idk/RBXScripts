ffd6bd25dc8d77a0134e194c73b82cbc67f07f34
your4eyes <99973263+your4eyes@users.noreply.github.com>
2022-07-02 14:04:36 -0400

    Update Net_Library.lua

--- Net_Library.lua
+++ Net_Library.lua
@@ -24,73 +24,69 @@ if not getgenv().Network then
 		Connections = {};
 		Output = {
 			Enabled = true;
+			Prefix = "[NETWORK] ";
 			Send = function(Type,Output,BypassOutput)
-				if type(Type) == "function" and (Type == print or Type == warn or Type == error) and type(Output) == "string" and (type(BypassOutput) == "nil" or type(BypassOutput) == "boolean") then
+				if typeof(Type) == "function" and (Type == print or Type == warn or Type == error) and typeof(Output) == "string" and (typeof(BypassOutput) == "nil" or typeof(BypassOutput) == "boolean") then
 					if Network["Output"].Enabled == true or BypassOutput == true then
-						Type("[NETWORK] "..Output);
+						Type(Network["Output"].Prefix..Output);
 					end;
 				elseif Network["Output"].Enabled == true then
-					error("[NETWORK] Output Send Error : Invalid syntax.");
+					error(Network["Output"].Prefix.."Output Send Error : Invalid syntax.");
 				end;
 			end;
 		};
 		CharacterRelative = false;
 	}
-	Network["Velocity"] = Vector3.new(17.3205081,17.3205081,17.3205081); --exactly 30 magnitude
+
+	Network["Output"].Send(print,": Loading.")
+	Network["Velocity"] = Vector3.new(14.46262424,14.46262424,14.46262424); --exactly 25.1 magnitude
 	Network["RetainPart"] = function(Part,ReturnFakePart) --function for retaining ownership of unanchored parts
-		if Part ~= nil and type(Part) == "userdata" and Part:IsA("BasePart") and (type(ReturnFakePart) == "boolean" or type(ReturnFakePart) == "nil") then
-			if Part:IsDescendantOf(workspace) then
-				if not table.find(Network["BaseParts"],Part) then
-					if Network.CharacterRelative == true then
-						local Character = LocalPlayer.Character
-						if Character and Character.PrimaryPart then
-							local Distance = (Character.PrimaryPart.Position-Part.Position).Magnitude
-							if Distance > 1000 then
-								Network["Output"].Send(warn,"RetainPart Warning : PartOwnership not applied to BasePart "..Part:GetFullName()..", as it is more than "..gethiddenproperty(LocalPlayer,"MaximumSimulationRadius").." studs away.")
-								return
-							end
-						else
-							Network["Output"].Send(warn,"RetainPart Warning : PartOwnership not applied to BasePart "..Part:GetFullName()..", as the LocalPlayer Character's PrimaryPart does not exist.")
-							return
-						end
-					end
-					table.insert(Network["BaseParts"],Part)
-					if ReturnFakePart == true then
-						return FakePart
+		assert(typeof(Part) == "Instance" and Part:IsA("BasePart") and Part:IsDescendantOf(workspace),Network["Output"].Prefix.."RetainPart Error : Invalid syntax: Arg1 (Part) must be a BasePart which is a descendant of workspace.")
+		assert(typeof(ReturnFakePart) == "boolean" or typeof(ReturnFakePart) == "nil",Network["Output"].Prefix.."RetainPart Error : Invalid syntax: Arg2 (ReturnFakePart) must be a boolean or nil.")
+		if not table.find(Network["BaseParts"],Part) then
+			if Network.CharacterRelative == true then
+				local Character = LocalPlayer.Character
+				if Character and Character.PrimaryPart then
+					local Distance = (Character.PrimaryPart.Position-Part.Position).Magnitude
+					if Distance > 1000 then
+						Network["Output"].Send(warn,"RetainPart Warning : PartOwnership not applied to BasePart "..Part:GetFullName()..", as it is more than "..gethiddenproperty(LocalPlayer,"MaximumSimulationRadius").." studs away.")
+						return false
 					end
 				else
-					Network["Output"].Send(warn,"RetainPart Warning : PartOwnership not applied to BasePart "..Part:GetFullName()..", as it already active.")
+					Network["Output"].Send(warn,"RetainPart Warning : PartOwnership not applied to BasePart "..Part:GetFullName()..", as the LocalPlayer Character's PrimaryPart does not exist.")
+					return false
 				end
-			else
-				Network["Output"].Send(error,"RetainPart Error : Instance "..tostring(Part).." is not a BasePart or does not exist in workspace.")
+			end
+			table.insert(Network["BaseParts"],Part)
+			Part.CustomPhysicalProperties = PhysicalProperties.new(0,0,0,0,0)
+			Network["Output"].Send(print,"PartOwnership Output : PartOwnership applied to BasePart "..Part:GetFullName()..".")
+			if ReturnFakePart == true then
+				return FakePart
 			end
 		else
-			Network["Output"].Send(error,"RetainPart Error : Invalid syntax.")
+			Network["Output"].Send(warn,"RetainPart Warning : PartOwnership not applied to BasePart "..Part:GetFullName()..", as it already active.")
+			return false
 		end
 	end
+
 	Network["RemovePart"] = function(Part) --function for removing ownership of unanchored part
-		if Part ~= nil and type(Part) == "userdata" and Part:IsA("BasePart")  then
-			if Part:IsA("BasePart") then
-				local Index = table.find(Network["BaseParts"],Part)
-				if Index then
-					table.remove(Network["BaseParts"],Index)
-					Network["Output"].Send(print,"RemovePart Output: PartOwnership removed from part "..Part:GetFullName()..".")
-				else
-					Network["Output"].Send(warn,"RemovePart Warning : BasePart "..Part:GetFullName().." not found in BaseParts table.")
-				end
-			else
-				Network["Output"].Send(error,"RemovePart Error : Instance "..tostring(Part).." is not a BasePart or does not exist in workspace or in game.")
-			end
+		assert(typeof(Part) == "Instance" and Part:IsA("BasePart"),Network["Output"].Prefix.."RemovePart Error : Invalid syntax: Arg1 (Part) must be a BasePart.")
+		local Index = table.find(Network["BaseParts"],Part)
+		if Index then
+			table.remove(Network["BaseParts"],Index)
+			Network["Output"].Send(print,"RemovePart Output: PartOwnership removed from BasePart "..Part:GetFullName()..".")
 		else
-			Network["Output"].Send(error,"RemovePart Error : Invalid syntax.")
+			Network["Output"].Send(warn,"RemovePart Warning : BasePart "..Part:GetFullName().." not found in BaseParts table.")
 		end
 	end
+
 	Network["SuperStepper"] = Instance.new("BindableEvent") --make super fast event to connect to
 	for _,Event in pairs({RunService.Stepped,RunService.Heartbeat}) do
 		Event:Connect(function()
 			return Network["SuperStepper"]:Fire(Network["SuperStepper"],tick())
 		end)
 	end
+
 	Network["PartOwnership"] = {};
 	Network["PartOwnership"]["PreMethodSettings"] = {};
 	Network["PartOwnership"]["Enabled"] = false;
@@ -105,7 +101,6 @@ if not getgenv().Network then
 				for _,Part in pairs(Network["BaseParts"]) do --loop through parts and do network stuff
 					coroutine.wrap(function()
 						if Part:IsDescendantOf(workspace) then
-							local Lost = false;
 							if Network.CharacterRelative == true then
 								local Character = LocalPlayer.Character;
 								if Character and Character.PrimaryPart then
@@ -117,17 +112,14 @@ if not getgenv().Network then
 									end
 								else
 									Network["Output"].Send(warn,"PartOwnership Warning : PartOwnership not applied to BasePart "..Part:GetFullName()..", as the LocalPlayer Character's PrimaryPart does not exist.")
-									Lost = true;
 								end
 							end
-							if Lost == false then
-								Part.Velocity = Network["Velocity"]+Vector3.new(0,math.cos(tick()*10)/10,0) --keep network by sending physics packets of 30 magnitude + an everchanging addition in the y level so roblox doesnt get triggered and fuck your ownership
-								if not isnetworkowner(Part) then --lag parts my ownership is contesting but dont have network over to spite the people who have ownership of stuff i want >:(
-									--Network["Output"].Send(warn,"PartOwnership Warning : Part "..Part:GetFullName().." is not owned. Contesting ownership...") --you can comment this out if you dont want console spam lol
-									sethiddenproperty(Part,"NetworkIsSleeping",true)
-								else
-									sethiddenproperty(Part,"NetworkIsSleeping",false)
-								end
+							Part.Velocity = Network["Velocity"]--+Vector3.new(0,math.cos(tick()*10)/100,0) --keep network by sending physics packets of 30 magnitude + an everchanging addition in the y level so roblox doesnt get triggered and fuck your ownership
+							if isnetworkowner(Part) == false then --lag parts my ownership is contesting but dont have network over to spite the people who have ownership of stuff i want >:(
+								--Network["Output"].Send(warn,"PartOwnership Warning : Part "..Part:GetFullName().." is not owned. Contesting ownership...") --you can comment this out if you dont want console spam lol
+								sethiddenproperty(Part,"NetworkIsSleeping",true)
+							else
+								sethiddenproperty(Part,"NetworkIsSleeping",false)
 							end
 						else
 							Network["RemovePart"](Part)
@@ -137,6 +129,8 @@ if not getgenv().Network then
 				end
 			end)
 			Network["Output"].Send(print,"PartOwnership Output : PartOwnership enabled.")
+		else
+			Network["Output"].Send(warn,"PartOwnership Output : PartOwnership already enabled.")
 		end
 	end)
 	Network["PartOwnership"]["Disable"] = coroutine.create(function()
@@ -150,6 +144,9 @@ if not getgenv().Network then
 			end
 			Network["PartOwnership"]["Enabled"] = false
 			Network["Output"].Send(print,"PartOwnership Output : PartOwnership disabled.")
+		else
+			Network["Output"].Send(warn,"PartOwnership Output : PartOwnership already disabled.")
 		end
 	end)
+	Network["Output"].Send(print,": Loaded.")
 end
